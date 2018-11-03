c17b6884190c5a4fc3ecd2c5719687576304241b
rikuya6 <rikuya6@gmail.com>
2018-11-04 00:44:30 +0900

    Heroku対応

--- db/seeds/production/users.rb
+++ db/seeds/production/users.rb
@@ -10,16 +10,3 @@ User.create!(
   password: 'password',
   password_confirmation: 'password'
 )
-
-names = []
-3.times do
-  names <<  Faker::Internet.user_name(Faker::StarWars.character, %w(. _ -))
-end
-
-names.each do |name|
-  User.create!(
-    email: Faker::Internet.safe_email(name),
-    password: 'password',
-    password_confirmation: 'password'
-  )
-end
