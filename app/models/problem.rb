921f4572585a27ce8259300423f80652acbafdae
rikuya6 <rikuya6@gmail.com>
2018-12-09 13:28:59 +0900

    PROX法実装

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -46,25 +46,75 @@ class Problem < ApplicationRecord
 
   # クラスメソッド
   def self.estimate_prox
-    # 項目困難度の線形化
-    # 誤答のログ・オッズを求める
-    user_count = User.all.count # 受験者数　@TODO 受験者を誰にするか決める。
+    item = calc_init_item_calibrations # 初期項目困難度
+    # p '初期項目困難度', item[:init_item_calibrations]
+    person = calc_init_person_measures # 初期受験者能力
+    # p '初期受験者能力', person[:init_person_measures]
+    item_u = calc_unbiased_variance(item[:incorrect_odds], item[:incorrect_odds_mean]) # 項目困難度の不偏分散: 応答データのばらつき度合いを求める
+    # p '項目困難度の不偏分散', item_u
+    person_u = calc_unbiased_variance(person[:init_person_measures], person[:init_person_measures_mean]) # 受験者能力の不偏分散
+    # p '受験者能力の不偏分散', person_u
+    item_finals = calc_final_calibrations(person_u, item_u, item[:init_item_calibrations])   # 最終項目困難度
+    # p '最終項目困難度', item_finals
+    person_finals = calc_final_calibrations(item_u, person_u, person[:init_person_measures]) # 最終受験者能力
+    # p '最終受験者能力', person_finals
+  end
+
+  def self.calc_init_item_calibrations
+    # 初期項目困難度を計算する
+    # 初めに、項目困難度の線形化し誤答のログ・オッズを求める
+    user_count = User.all.count # 受験者数　@TODO 受験者を誰にするか決める,
     problem_count = Problem.count
-    wrong_odds = [] # 誤答のログ・オッズ
+    incorrect_odds = [] # 誤答のログ・オッズ
     Problem.all.each do |problem|
       correct_count = problem.user_problems.count
       correct_rate = correct_count / user_count.to_f # 正答数
-      wrong_rate = 1 - correct_rate                  # 誤答率
-      wrong_odds << Math.log(wrong_rate / correct_rate)
+      incorrect_rate = 1 - correct_rate              # 誤答率
+      incorrect_odds << Math.log(incorrect_rate / correct_rate)
     end
-    # p wrong_odds
+    # p incorrect_odds
     # 初期項目困難度の計算
-    wrong_odds_mean = wrong_odds.sum / problem_count # ログ・オッズの平均値
-    init_item_calibrations = wrong_odds.map { |w| w - wrong_odds_mean } # 初期項目困難度
-    # p init_item_calibrations
+    item = {}
+    item[:incorrect_odds] = incorrect_odds
+    item[:incorrect_odds_mean] = incorrect_odds.sum / problem_count # ログ・オッズの平均値
+    item[:init_item_calibrations] = incorrect_odds.map { |w| w - item[:incorrect_odds_mean] } # 初期項目困難度
+
+    item
   end
+  private_class_method :calc_init_item_calibrations
+
+  def self.calc_init_person_measures
+    # 正答のログ・オッズを計算する。計算結果が、初期受験者能力となる。
+    user_count = User.all.count # @TODO 受験者を誰にするか決める
+    correct_odds = [] # 正答のログ・オッズ
+    User.all.each do |user|
+      correct_count = user.user_problems.count
+      correct_rate = correct_count / (user_count - 1).to_f # 正答率。 (受験者数 - 1)とする
+      incorrect_rate = 1 - correct_rate                    # 誤答率
+      correct_odds << Math.log(correct_rate / incorrect_rate)
+    end
+    correct_odds_mean = correct_odds.sum / user_count.to_f
+    # 初期受験者能力。ただし、初期項目困難度の計算時に原点を定める作業を行っている場合に限る
+    { init_person_measures: correct_odds, init_person_measures_mean: correct_odds_mean }
+  end
+  private_class_method :calc_init_person_measures
+
+  def self.calc_unbiased_variance(vers, mean)
+    vers.map { |ver| (ver - mean) ** 2 }.sum / (vers.size - 1) # 不偏分散
+  end
+  private_class_method :calc_unbiased_variance
 
 
+  def self.calc_final_calibrations(u, v, init_items)
+    ef = expansion_factor(u, v)
+    init_items.map { |item| item * ef }
+  end
+  private_class_method :calc_final_calibrations
+
+  def self.expansion_factor(u, v)
+    Math.sqrt((1 + u / 2.89) / (1 - u * v / 8.35)) # 2.89は1.7を2乗した値。8.35は1.7を4乗した値。
+  end
+  private_class_method :expansion_factor
 
 
   def self.recommend(user)
